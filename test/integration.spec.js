53c655fbf16c0b730c57b763a6b9a9ce10feb2a6
lykmapipo <lallyelias87@gmail.com>
2019-04-20 21:26:28 +0300

    feat: add additonal hooks to error middleware

--- test/integration.spec.js
+++ test/integration.spec.js
@@ -6,7 +6,7 @@ process.env.NODE_ENV = 'test';
 
 
 /* dependencies */
-const { waterfall } = require('async');
+// const { waterfall } = require('async');
 const { include } = require('@lykmapipo/include');
 const { expect } = require('chai');
 const {
@@ -31,15 +31,36 @@ describe('common', () => {
 
     // wait index
     User.on('index', () => {
-      waterfall([
-        next => User.create(user, error => next(error)),
-        next => User.create(user, error => next(error))
-      ], error => {
+      User.create([user, user], error => {
         expect(error).to.exist;
         expect(error.status).to.exist;
         expect(error.name).to.exist;
         expect(error.name).to.be.equal('ValidationError');
         expect(error._message).to.exist;
+        expect(error.message).to.exist;
+        expect(error.errors).to.exist;
+        expect(error.errors.name).to.exist;
+        expect(error.errors.name.kind).to.exist;
+        expect(error.errors.name.kind).to.be.equal('unique');
+        done();
+      });
+    });
+  });
+
+  it('should beautify unique error message on insertMany', done => {
+    const schema = new Schema({ name: { type: String, unique: true } });
+    const User = model(schema);
+    const user = { name: 'John Doe' };
+
+    // wait index
+    User.on('index', () => {
+      User.insertMany([user, user], error => {
+        expect(error).to.exist;
+        expect(error.status).to.exist;
+        expect(error.name).to.exist;
+        expect(error.name).to.be.equal('ValidationError');
+        expect(error._message).to.exist;
+        expect(error.message).to.exist;
         expect(error.errors).to.exist;
         expect(error.errors.name).to.exist;
         expect(error.errors.name.kind).to.exist;
@@ -57,15 +78,42 @@ describe('common', () => {
 
     // wait index
     User.on('index', () => {
-      waterfall([
-        next => User.create(user, error => next(error)),
-        next => User.create(user, error => next(error))
-      ], error => {
+      User.create([user, user], error => {
         expect(error).to.exist;
         expect(error.status).to.exist;
         expect(error.name).to.exist;
         expect(error.name).to.be.equal('ValidationError');
         expect(error._message).to.exist;
+        expect(error.message).to.exist;
+        expect(error.errors).to.exist;
+        expect(error.errors.firstName).to.exist;
+        expect(error.errors.firstName.kind).to.exist;
+        expect(error.errors.firstName.kind)
+          .to.be.equal('unique');
+        expect(error.errors.lastName).to.exist;
+        expect(error.errors.lastName.kind).to.exist;
+        expect(error.errors.lastName.kind)
+          .to.be.equal('unique');
+        done();
+      });
+    });
+  });
+
+  it('should beautify compound unique error message on insertMany', done => {
+    const schema = new Schema({ firstName: String, lastName: String });
+    schema.index({ firstName: 1, lastName: 1 }, { unique: true });
+    const User = model(schema);
+    const user = { firstName: 'John', lastName: 'Doe' };
+
+    // wait index
+    User.on('index', () => {
+      User.insertMany([user, user], error => {
+        expect(error).to.exist;
+        expect(error.status).to.exist;
+        expect(error.name).to.exist;
+        expect(error.name).to.be.equal('ValidationError');
+        expect(error._message).to.exist;
+        expect(error.message).to.exist;
         expect(error.errors).to.exist;
         expect(error.errors.firstName).to.exist;
         expect(error.errors.firstName.kind).to.exist;
